fd9b74d3e3fc59f33225a7b1572b17799e780a09
yuriyberezskyy <bereza95@gmail.com>
2020-03-26 13:58:15 -0400

    done

---
+++ manupulate_with_strings.py
@@ -0,0 +1,9 @@
+course = "   Python is the best"
+print(course.upper())  # all uppercase letters
+print(course.lower())  # all lowercase letters
+print(course.title())  # put all capitals on each word
+print(course.strip())  # take away the space from begging of the string
+print(course.find("is"))  # finding an index(position) of the exact search
+print(course.replace("P", "-"))  # replacing all "P" with "-"
+print("Python" in course)  # boolean finding word in sentence
+print("Python" not in course)  # boolean finding word in sentence
